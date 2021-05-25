41cf0f5ffa994ae94a4045317c35cfa174fac100
smarqueslopez <smarqueslopez@gmail.com>
2021-05-25 21:10:05 +0200

    feat(global): :tada: Initial commit
    Adding blank base project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>React Trello clone</title>
+        <meta name='description' content='React Trello clone' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  )
+}
