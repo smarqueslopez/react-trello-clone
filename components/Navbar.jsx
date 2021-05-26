9b37df63b6e0132b1f959d111a66481a1faa3afc
smarqueslopez <smarqueslopez@gmail.com>
2021-05-26 19:23:07 +0200

    refactor(global): :heavy_minus_sign: Remove imports
    Removed react import in components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { IconButton } from '@material-ui/core'
-import React from 'react'
 import styled from 'styled-components'
 import DashboardIcon from '@material-ui/icons/Dashboard'
 import MenuIcon from '@material-ui/icons/Menu'
